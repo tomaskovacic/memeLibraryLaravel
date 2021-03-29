a5325c3b128d65004d22e016e360347e59ed184b
tomas <tomas.kovacic1@gmail.com>
2021-03-29 13:19:59 +0200

    first commit

---
+++ laravel-vue-spa/resources/js/pages/welcome.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div class="top-right links">
+      <template v-if="authenticated">
+         <router-link :to="{ name: 'welcome' }">
+          {{ $t('home') }}
+        </router-link>
+        <router-link :to="{ name: 'home' }">
+          {{ $t('Profile') }}
+        </router-link>
+         <router-link :to="{ name: 'post' }">
+          {{ $t('Post Meme') }}
+        </router-link>
+      </template>
+      <template v-else>
+        <router-link :to="{ name: 'welcome' }">
+          {{ $t('home') }}
+        </router-link>
+        <router-link :to="{ name: 'login' }">
+          {{ $t('login') }}
+        </router-link>
+        <router-link :to="{ name: 'register' }">
+          {{ $t('register') }}
+        </router-link>
+      </template>
+    </div>
+
+    <div class="text-center">
+      <div class="title mb-4">
+        {{ title }}
+      </div>
+
+      <div class="links">
+        <b>Meme Library</b>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  layout: 'basic',
+
+  metaInfo () {
+    return { title: this.$t('home') }
+  },
+
+  data: () => ({
+    title: window.config.appName
+  }),
+
+  computed: mapGetters({
+    authenticated: 'auth/check'
+  })
+}
+</script>
+
+<style scoped>
+.top-right {
+  position: absolute;
+  right: 10px;
+  top: 18px;
+}
+
+.title {
+  font-size: 85px;
+}
+</style>
